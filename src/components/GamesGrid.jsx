0263f0bd7f312c62d960c6286d5b1a822b468554
Victor <visama396@gmail.com>
2025-09-14 22:42:24 +0200

    Update GamesGrid.jsx

--- src/components/GamesGrid.jsx
+++ src/components/GamesGrid.jsx
@@ -4,62 +4,49 @@ function GamesGrid({ apikey }) {
     const baseRAWG = `https://api.rawg.io/api/games?key=${apikey}`
     let searchRAWG = baseRAWG;
     const [gameNameFilter, setGameNameFilter] = useState("")
+    const [searchUrlRawg, setSearchRawgUrl] = useState(baseRAWG)
+    const [filteredGames, setFilteredGames] = useState([])
 
     const handleChange = event => {
         setGameNameFilter(event.target.value)
+        setSearchRawgUrl(baseRAWG + `?search=${event.target.value}`)
+        searchGamesByName()
+    }
+
+    const handleSubmit = event => {
+
+    }
+
+    const searchGamesByName = async () => {
+        await fetch(searchUrlRawg).then(response => console.log(response)).catch(err => console.error(err))
     }
 
     return (
         <section>
             <div>
-                <form class="max-w-lg mx-auto">
-                    <div class="flex">
-                        <label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">Your Email</label>
-                        <button id="dropdown-button" data-dropdown-toggle="dropdown" class="shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600" type="button">
-                            Todos los géneros 
-                            <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                <form className="max-w-lg mx-auto flex flex-col gap-2">
+                    <div className="flex px-4 py-2 rounded-lg gap-3 bg-gray-700">
+                        <select id="dropdown" className="bg-gray-700">
+                            <option value="Action">Action</option>
+                            <option value="Adventure">Adventure</option>
+                            <option value="Racing">Racing</option>
+                            <option value="MMO">MMO</option>
+                        </select>
+                        <input onChange={handleChange} value={gameNameFilter} type="search" id="search-dropdown" className="flex-1 outline-0" placeholder="Search games by name" required />
+                        <button onClick={handleSubmit} type="submit" className="hover:cursor-pointer">
+                            <svg className="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
                             </svg>
                         </button>
-                    <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700">
-                        <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdown-button">
-                            <li>
-                                <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Mockups</button>
-                            </li>
-                            <li>
-                                <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Templates</button>
-                            </li>
-                            <li>
-                                <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Design</button>
-                            </li>
-                            <li>
-                                <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Logos</button>
-                            </li>
-                        </ul>
                     </div>
-                    <div class="relative w-full">
-                        <input type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500" placeholder="Search Mockups, Logos, Design Templates..." required />
-                        <button type="submit" class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
-                    </svg>
-                    <span class="sr-only">Search</span>
-                </button>
+                    {filteredGames.length > 0 && (
+                        <div className='px-4 py-1 bg-gray-700 rounded-lg'>
+                            <h2>Game bla bla</h2>
+                        </div>
+                    )}
+                </form>    
             </div>
-        </div>
-    </form>    
-
-
-            </div>
-            <h2>Searching for: {gameNameFilter}</h2>
-            <input 
-                type="text" 
-                onChange={handleChange} 
-                value={gameNameFilter} 
-                name="gameFilter" 
-                placeholder="What game are you looking for?" 
-                className="border border-grey"
-            />
+            <h2>Searching for: {searchUrlRawg}</h2>
         </section>
     )
 }
